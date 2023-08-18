46b33bc331fd2ac8ee24d436f863fe3622eaf85e
davidyu <lewen.yu@gmail.com>
2023-08-18 09:22:17 -0700

    more infra boilerplate

--- fizz.asm
+++ fizz.asm
@@ -27,11 +27,74 @@ WaitVBlank:
     ld bc, TilemapEnd - Tilemap
     call Memcopy
 
+ClearOam:
+    ld [hli], a
+    dec b
+    jp nz, ClearOam
+
+    ld hl, _OAMRAM
+    ld a, 128 + 16
+    ld [hli], a
+    ld a, 16 + 8
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+    ld [hl], a
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
+    ld [rLCDC], a
+
+    ; During the first (blank) frame, initialize display registers
+    ld a, %11100100
+    ld [rBGP], a
+    ld a, %11100100
+    ld [rOBP0], a
+
 Main:
     ; Wait until it's *not* VBlank
     ld a, [rLY]
     cp 144
     jp nc, Main
+
+WaitVBlank2:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank2
+
+    call UpdateKeys
+
+CheckLeft:
+    ld a, [wCurKeys]
+    and a, PADF_LEFT
+    jp z, CheckRight
+
+Left:
+    ; Move the paddle one pixel to the left.
+    ld a, [_OAMRAM + 1]
+    dec a
+    dec a
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 15
+    jp z, Main
+    ld [_OAMRAM + 1], a
+    jp Main
+
+; Then check the right button.
+CheckRight:
+    ld a, [wCurKeys]
+    and a, PADF_RIGHT
+    jp z, Main
+Right:
+    ; Move the paddle one pixel to the right.
+    ld a, [_OAMRAM + 1]
+    inc a
+    inc a
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 105
+    jp z, Main
+    ld [_OAMRAM + 1], a
+    jp Main
 
 ; Copy bytes from one area to another.
 ; @param de: Source
@@ -47,8 +110,50 @@ Memcopy:
     jp nz, Memcopy
     ret
 
+
+UpdateKeys:
+    ; Poll half the controller
+    ld a, P1F_GET_BTN
+    call .onenibble
+    ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
+
+    ; Poll the other half
+    ld a, P1F_GET_DPAD
+    call .onenibble
+    swap a ; A3-0 = unpressed directions; A7-4 = 1
+    xor a, b ; A = pressed buttons + directions
+    ld b, a ; B = pressed buttons + directions
+
+    ; And release the controller
+    ld a, P1F_GET_NONE
+    ldh [rP1], a
+
+    ; Combine with previous wCurKeys to make wNewKeys
+    ld a, [wCurKeys]
+    xor a, b ; A = keys that changed state
+    and a, b ; A = keys that changed to pressed
+    ld [wNewKeys], a
+    ld a, b
+    ld [wCurKeys], a
+    ret
+
+.onenibble
+    ldh [rP1], a ; switch the key matrix
+    call .knownret ; burn 10 cycles calling a known ret
+    ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
+    ldh a, [rP1]
+    ldh a, [rP1] ; this read counts
+    or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
+.knownret
+    ret
+
+
 Tiles:
 TilesEnd:
 
 Tilemap:
 TilemapEnd:
+
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
